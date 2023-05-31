b21d2174bfb7aa80cce6536cafa1bf7b735544fe
cswbyu <wb_yu@foxmail.com>
2023-05-31 17:41:17 +0800

    finish hw3

--- Homework3/segmentation.py
+++ Homework3/segmentation.py
@@ -39,17 +39,87 @@ def kmeans(features, k, num_iters=500):
     for n in range(num_iters):
         ### YOUR CODE HERE
         pass
+        stop_flag = True
+        for feature_idx in range(N): # 遍历每一个点
+            feature = features[feature_idx] # 当前遍历到的点
+            assignment = 0
+            center = centers[0]
+            distance = np.sum(np.square(feature - center))
+            for center_idx in range(1, k): # 寻找距离最小的中心点，将当前的点归为该 cluster
+                new_center = centers[center_idx]
+                new_distance = np.sum(np.square(feature - new_center))
+                if new_distance < distance:
+                    assignment = center_idx
+                    distance = new_distance
+                    center = new_center
+            if assignments[feature_idx] != assignment:
+                stop_flag = False # cluster assignments 没有变化时才提前结束
+                assignments[feature_idx] = assignment
+
+        for center_idx in range(k): # 更新每一个 cluster 的中心
+            center_features = [] # 当前 cluster 所管辖的所有点
+            for feature_idx in range(N):
+                if assignments[feature_idx] == center_idx:
+                    center_features.append(features[feature_idx])
+            center_features = np.array(center_features)
+            if center_features.shape[0] == 0:
+                continue
+            centers[center_idx, 0] = np.mean(center_features[:, 0])
+            centers[center_idx, 1] = np.mean(center_features[:, 1])
+
+        if stop_flag:
+            break
         ### END YOUR CODE
 
     return assignments
 
 ### Clustering Methods for colorful image
 def kmeans_color(features, k, num_iters=500):
-    N=None # 像素个数
+    new_features = features.reshape(-1, 3)
+    H, W = features.shape[0], features.shape[1]
+    N = H * W
     assignments = np.zeros(N, dtype=np.uint32)
     #Like the kmeans function above
     ### YOUR CODE HERE
     pass
+    idxs = np.random.choice(N, size=k, replace=False)
+    centers = (new_features[idxs]).astype(np.float32)
+
+    for n in range(num_iters):
+        stop_flag = True
+
+        for feature_idx in range(N): # 遍历每一个点
+            feature = new_features[feature_idx] # 当前遍历到的点
+            assignment = 0
+            center = centers[0]
+            distance = np.sum(np.square(feature - center))
+            for center_idx in range(1, k): # 寻找距离最小的中心点，将当前的点归为该 cluster
+                new_center = centers[center_idx]
+                new_distance = np.sum(np.square(feature - new_center))
+                if new_distance < distance:
+                    assignment = center_idx
+                    distance = new_distance
+                    center = new_center
+            if assignments[feature_idx] != assignment:
+                stop_flag = False # cluster assignments 没有变化时才提前结束
+                assignments[feature_idx] = assignment
+
+        for center_idx in range(k): # 更新每一个 cluster 的中心
+            center_features = [] # 当前 cluster 所管辖的所有点
+            for feature_idx in range(N):
+                if assignments[feature_idx] == center_idx:
+                    center_features.append(new_features[feature_idx])
+            center_features = np.array(center_features)
+            if center_features.shape[0] == 0:
+                continue
+            centers[center_idx, 0] = np.mean(center_features[:, 0])
+            centers[center_idx, 1] = np.mean(center_features[:, 1])
+            centers[center_idx, 2] = np.mean(center_features[:, 2])
+
+        if stop_flag:
+            break
+    
+    assignments = assignments.reshape(H, W)
     ### END YOUR CODE
 
     return assignments
@@ -68,12 +138,19 @@ def findpeak(data, idx, r):
     data_pointT = data_pointT.reshape(1, 3)
 
     # Runs until the shift is smaller than the set threshold
-    while shift.all() > t:
-        # 计算当前点和所有点之间的距离
-        # 并筛选出在半径r内的点，计算mean vector（这里是最简单的均值，也可尝试高斯加权）
-        # 用新的center（peak）更新当前点，直到满足要求跳出循环
+    while (shift > t).any():
         ### YOUR CODE HERE
         pass
+        # 计算当前点和所有点之间的距离
+        distance = np.sqrt(np.sum(np.square(data_pointT - dataT), axis=1, keepdims=True))
+
+        # 筛选出在半径r内的点，计算mean vector（这里是最简单的均值，也可尝试高斯加权）
+        filtered_points = dataT[(np.where(distance <= r))[0]]
+        mean_vector = np.mean(filtered_points, axis=0)
+        shift = np.sqrt(np.sum(np.square(mean_vector - data_pointT)))
+
+        # 用新的center（peak）更新当前点，直到满足要求跳出循环
+        data_pointT = mean_vector.reshape((1, 3))
         ### END YOUR CODE
 
     return data_pointT.T
@@ -94,12 +171,24 @@ def meanshift(data, r):
 
     # Every data point is iterated through
     for idx in range(0, len(data.T)):
-        # 遍历数据，寻找当前点的peak
-        # 并实时关注当前peak是否会收敛到一个新的聚类（和已有peaks比较）
-        # 若是，更新label_no，peaks，labels，继续
-        # 若不是，当前点就属于已有类，继续
         ### YOUR CODE HERE
         pass
+        # 遍历数据，寻找当前点的peak
+        peak = np.concatenate(findpeak(data, idx, r), axis=0).T
+
+        # 并实时关注当前peak是否会收敛到一个新的聚类（和已有peaks比较）
+        new_peak_flag = True
+        for i in range(len(peaks)):
+            if np.sqrt(np.sum(np.square(peak - peaks[i]))) < r:
+                new_peak_flag = False
+        if new_peak_flag: # 若是，更新label_no，peaks，labels，继续
+            label_no += 1
+            peaks.append(peak)
+            labels[idx] = label_no
+        else: # 若不是，当前点就属于已有类，继续
+            for i in range(len(peaks)):
+                if np.sqrt(np.sum(np.square(peak - peaks[i]))) < r:
+                    labels[idx] = i + 1
         ### END YOUR CODE
     #print(set(labels))
     return labels, np.array(peaks).T
@@ -156,6 +245,13 @@ def compute_accuracy(mask_gt, mask):
     accuracy = None
     ### YOUR CODE HERE
     pass
+    # 计算正反两种情况的准确率，然后取最大值
+    total_counts = mask_gt.size
+    correct_counts_1 = np.count_nonzero(mask_gt == mask)
+    accuracy_1 = correct_counts_1 / total_counts
+    correct_counts_2 = np.count_nonzero(mask_gt != mask)
+    accuracy_2 = correct_counts_2 / total_counts
+    accuracy = max(accuracy_1, accuracy_2)
     ### END YOUR CODE
 
     return accuracy
